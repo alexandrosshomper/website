3de418ca0b27da8489efb75ec7243236b01ef201
Alexandros Shomper <53514717+alexandrosshomper@users.noreply.github.com>
2025-09-17 22:17:46 +0200

    Fix sticky behavior and improve tests

--- src/hooks/useSticky.js
+++ src/hooks/useSticky.js
@@ -1,37 +1,27 @@
-import { useEffect, useState, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function useSticky() {
   const [isSticky, setSticky] = useState(false);
   const element = useRef(null);
 
-  const handleScroll = () => {
-    window.scrollY > element.current.getBoundingClientRect().bottom
-      ? setSticky(true)
-      : setSticky(false);
-  };
+  const handleScroll = useCallback(() => {
+    if (!element.current) {
+      setSticky(false);
+      return;
+    }
 
-  const debounce = (func, wait = 20, immediate = true) => {
-    let timeout;
-    return () => {
-      let context = this;
-      let args = arguments;
-      const later = () => {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
+    const { bottom } = element.current.getBoundingClientRect();
+    setSticky(bottom <= 0);
+  }, []);
 
   useEffect(() => {
-    window.addEventListener("scroll", debounce(handleScroll));
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
     return () => {
-      window.removeEventListener("scroll", () => handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
-  }, [debounce, handleScroll]);
+  }, [handleScroll]);
 
   return { isSticky, element };
 }
