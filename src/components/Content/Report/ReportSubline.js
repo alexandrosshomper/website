9bfcd1152bc725af3156d738ceeac34cd56dd7d3
Alexandros Shomper <53514717+alexandrosshomper@users.noreply.github.com>
2025-09-28 19:03:28 +0200

    Align case and report headlines with shared styles

--- src/components/Content/Report/ReportSubline.js
+++ src/components/Content/Report/ReportSubline.js
@@ -1,39 +1,9 @@
 import React from "react";
-import styled from "@emotion/styled";
 
-import { Devices, Colors } from "../../DesignSystem";
+import { ArticleSubline } from "../../DesignSystem";
 
 const ReportSubline = ({ subline }) => {
-  const ReportSubline = styled.p`
-    font-family: "Roboto", sans-serif;
-    font-weight: 550;
-    font-style: normal;
-    letter-spacing: 0.04rem;
-
-    color: ${Colors.primaryText.mediumEmphasis};
-    margin-bottom: 8px;
-    margin-top: 0px;
-
-    font-size: 28px;
-    line-height: 112%;
-    text-align: left;
-    width: 90%;
-    ${Devices.tabletS} {
-      width: 520px;
-      font-size: 36px;
-      line-height: 112%;
-    }
-    ${Devices.tabletM} {
-      width: 520px;
-    }
-    ${Devices.laptopS} {
-      width: 650px;
-    }
-    ${Devices.laptopM} {
-    }
-  `;
-
-  return <ReportSubline>{subline}</ReportSubline>;
+  return <ArticleSubline>{subline}</ArticleSubline>;
 };
 
 export default ReportSubline;
