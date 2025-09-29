106bcb6c28a06c0b9f03264ec21d50462a0158d3
Alexandros Shomper <53514717+alexandrosshomper@users.noreply.github.com>
2025-09-29 09:48:39 +0200

    Refactor overview typography styles

--- src/components/Content/Case/CaseSectionSummary.js
+++ src/components/Content/Case/CaseSectionSummary.js
@@ -1,39 +1,11 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-import { Devices } from "../../DesignSystem";
-
-import Copy from "../List/ListSmallText/ListSmallTextCopy";
+import { OverviewPages } from "../../DesignSystem";
 
 const CaseSectionSummary = ({ copy }) => {
-  const CaseSectionSummary = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-
-    text-align: left;
-
-    margin: 0px 24px 60px 24px;
-    &:last-child {
-      margin-bottom: 0px;
-    }
-
-    //padding-right: 12px;
-
-    ${Devices.tabletS} {
-      width: 100%;
-      margin: 0px 0px 60px 0px;
-    }
-    ${Devices.tabletM} {
-      width: 90%;
-    }
-    ${Devices.laptop} {
-      width: 80%;
-    }
-  `;
-
   return (
-    <CaseSectionSummary>{copy && <Copy text={copy} />}</CaseSectionSummary>
+    <OverviewPages.Summary>
+      {copy ? <OverviewPages.Copy>{copy}</OverviewPages.Copy> : null}
+    </OverviewPages.Summary>
   );
 };
 
--- src/components/Content/Section/SectionCopy.js
+++ src/components/Content/Section/SectionCopy.js
@@ -1,50 +1,8 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-import { Devices, Colors } from "../../DesignSystem";
-
-const SectionCopyText = styled.p`
-  font-family: "Roboto", sans-serif;
-  font-style: normal;
-  font-weight: normal;
-  color: ${Colors.primaryText.highEmphasis};
-  margin: 0px 24px 24px 24px;
-
-  font-size: 20px;
-  line-height: 120%;
-  text-align: left;
-
-  ${Devices.tabletS} {
-    text-align: left;
-    width: 576px;
-  }
-  ${Devices.tabletM} {
-    width: 720px;
-
-    font-size: 36px;
-    line-height: 111%;
-    margin-bottom: 32px;
-    margin-left: 0px;
-    margin-right: 0px;
-  }
-  ${Devices.laptopS} {
-    width: 864px;
-
-    font-size: 36px;
-    line-height: 100%;
-    margin-bottom: 38px;
-  }
-  ${Devices.laptopM} {
-    width: 1152px;
-
-    font-size: 42px;
-    line-height: 113%;
-    margin-bottom: 46px;
-  }
-`;
+import { OverviewPages } from "../../DesignSystem";
 
 const SectionCopy = ({ copy }) => {
-  return <SectionCopyText>{copy}</SectionCopyText>;
+  return <OverviewPages.Copy>{copy}</OverviewPages.Copy>;
 };
 
 export default SectionCopy;
