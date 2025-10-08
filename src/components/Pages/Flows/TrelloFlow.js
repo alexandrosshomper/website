9ef3e4fb849511d1ab17913ddfb318571c06e91e
Alexandros Shomper <53514717+alexandrosshomper@users.noreply.github.com>
2025-10-08 21:09:47 +0200

    Remove unused related resources from TrelloFlow and WrikeFlow components

--- src/components/Pages/Flows/TrelloFlow.js
+++ src/components/Pages/Flows/TrelloFlow.js
@@ -3,19 +3,6 @@ import React from "react";
 import trelloFlow from "../../../data/flows/trello_flow.json";
 import FlowPageTemplate from "./FlowPageTemplate";
 
-const relatedResources = [
-  {
-    eyebrow: "Case Study",
-    eyebrowColor2: "#FFEAED",
-    eyebrowColor1: "#FD594A",
-    headline:
-      "Asana – How positioning for teams & enterprise impacts your segmentation, setup, and plan selection",
-    copy: "",
-    imgURL: "/img/case_studies/trello/Cover@2x.png",
-    link: "/case-studies/trello",
-  },
-];
-
 const AsanaFlow = () => (
   <FlowPageTemplate flowSlug="trello" screens={trelloFlow} />
 );
--- src/components/Pages/Flows/WrikeFlow.js
+++ src/components/Pages/Flows/WrikeFlow.js
@@ -3,19 +3,6 @@ import React from "react";
 import wrikeFlow from "../../../data/flows/wrike_flow.json";
 import FlowPageTemplate from "./FlowPageTemplate";
 
-const relatedResources = [
-  {
-    eyebrow: "Case Study",
-    eyebrowColor2: "#FFEAED",
-    eyebrowColor1: "#FD594A",
-    headline:
-      "Asana – How positioning for teams & enterprise impacts your segmentation, setup, and plan selection",
-    copy: "",
-    imgURL: "/img/case_studies/wrike/Cover@2x.png",
-    link: "/case-studies/wrike",
-  },
-];
-
 const AsanaFlow = () => (
   <FlowPageTemplate flowSlug="wrike" screens={wrikeFlow} />
 );
