abe22bec8f2c94562c654b542459d9f3c691b56f
Alexandros Shomper <53514717+alexandrosshomper@users.noreply.github.com>
2026-03-29 09:46:41 +0200

    refactor: update text and layout in various components
    - Revised text in Intro component to clarify expertise in product management.
    - Adjusted margins in HomeJob component for better layout consistency.
    - Simplified CaseBlockDetails in Portfolio components by merging elements and updating copy for clarity.
    - Cleaned up unnecessary code in KnaufMaterialCalculatorPMF and KnaufOrderOverview components.
    - Enhanced meta tags in Writing component for improved SEO and clarity.

--- src/components/Pages/Home/HomeJob.js
+++ src/components/Pages/Home/HomeJob.js
@@ -14,7 +14,6 @@ import FlipCard from "../../Content/FlipCard/FlipCard";
 import CaseStudyCardVideo from "../../Content/Case/CaseStudyCardVideo";
 import AiTechStack from "../../Content/AiTechStack/AiTechStack";
 
-
 const CASE_STUDY_TYPES = {
   ALL: "All Case Studies",
   ONBOARDING: "Onboarding Case Studies",
@@ -167,7 +166,7 @@ const CaseBlockDetails = styled.div`
   order: 1;
   text-align: left;
   ${Devices.tabletS} {
-    margin: 32px 0px 24px 24px;
+    margin: 24px 0px 24px 24px;
     order: 0;
     width: 40%;
   }
@@ -175,16 +174,6 @@ const CaseBlockDetails = styled.div`
 
 const CaseBlockDetailsText = styled.div`
   margin: 0px 0px 0px 0px;
-`;
-
-const CaseBlockEyebrow = styled.p`
-  color: ${Colors.primaryText.highEmphasis};
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 110%;
-
-  text-align: left;
-  margin: 0px 0px 4px 0px;
 `;
 
 const CaseBlockHeadline = styled.h3`
@@ -349,10 +338,9 @@ const Content = (props) => {
             >
               <CaseBlockDetails>
                 <CaseBlockDetailsText>
-                  <CaseBlockEyebrow style={{ color: caseStudy.color }}>
-                    {caseStudy.company}
-                  </CaseBlockEyebrow>
-                  <CaseBlockHeadline>{caseStudy.headline}</CaseBlockHeadline>
+                  <CaseBlockHeadline>
+                    {caseStudy.company} {caseStudy.headline}
+                  </CaseBlockHeadline>
                   <CaseBlockSubline>{caseStudy.subline}</CaseBlockSubline>
                   <CaseBlockCopy>{caseStudy.copy}</CaseBlockCopy>
                 </CaseBlockDetailsText>
